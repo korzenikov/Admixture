07fa63094593225b5ee0d05a5546b0d02a3ac980
Andrei Korzenikov <korzenikov@outlook.com>
2018-03-11 17:23:09 -0400

    more changes

--- Admixture/EthnoPlot.fsx
+++ Admixture/EthnoPlot.fsx
@@ -11,8 +11,11 @@
 open Admixture.Populations
 open Admixture.EthnoPlots
 
-let _, populations = getPopulations ',' "D:\Populations\K15.csv"
+let components, populations = getPopulations ',' "D:\Populations\K15.csv"
 
+(components, populations) ||> showEthnoPlot 5 -1.0 -0.25
+
+populations |> showEthnoPlotByClusters 5
 
 let testPersonK13 = 
     [|
@@ -31,7 +34,7 @@ let testPersonK13 =
         0.20
     |]
 
-let testPerson = 
+let testPersonK15 = 
     [|
         15.80
         18.44
@@ -50,31 +53,308 @@ let testPerson =
         0.0
     |]
 
-let testPerson2 = 
+let testPerson2K15 = 
     [|
-        21.24
-        15.18
-        31.29
-        21.55
-        4.30
-        2.35
-        2.59
-        0.0    
-        0.0    
-        0.0    
-        0.20
-        0.09
-        0.97
-        0.0    
-        0.25
+         21.24
+         15.18
+         31.29
+         21.55
+         4.30
+         2.35
+         2.59
+         0.0
+         0.0
+         0.0
+         0.20
+         0.09
+         0.97
+         0.0
+         0.25
+    |]
+
+let testPerson3K15 = 
+    [|
+        10.64
+        16.48
+        27.76
+        23.07
+        0.0
+        2.87
+        0.0
+        1.18
+        5.64
+        0.86
+        6.18
+        0.61
+        0.0
+        2.23
+        2.47
     |]
 
 
-showEthnoPlotOnly 5 populations
+let testPersonK36 = 
+    [|
+        0.54
+        0.0
+        0.0
+        1.72
+        0.0
+        10.19
+        0.0
+        0.0
+        3.41
+        0.0
+        23.37
+        0.0
+        22.47
+        16.11
+        0.0
+        1.03
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        8.25
+        0.5
+        5.03
+        0.0
+        0.0
+        0.49
+        0.0
+        0.29
+        1.2
+        0.0
+        0.0
+        5.4
+        0.0
+        0.0
+        0.0
+    |]
 
-(populations, testPerson) ||> showEthnoPlot 5 
+let testPerson2K36 = 
+    [|
+        0.0
+        0.0
+        0.0
+        1.43
+        0.0
+        12.65
+        0.0
+        0.0
+        5.35
+        0.0
+        24.12
+        0.0
+        20.32
+        9.96
+        0.0
+        0.0
+        0.0
+        6.24
+        0.0
+        0.0
+        0.0
+        9.11
+        0.0
+        7.88
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        2.93
+        0.0
+        0.0
+        0.0
+    |]
 
-(populations, testPerson) ||> showEthnoPlotSampleClusterOnly 5
+let testPerson3K36 = 
+    [|
+        0.77
+        0.0
+        0.0
+        1.29
+        0.0
+        7.31
+        1.31
+        0.29
+        2.02
+        0.0
+        20.48
+        0.0
+        21.75
+        17.01
+        0.0
+        0.57
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        6.88
+        0.0
+        2.17
+        0.0
+        0.0
+        1.24
+        0.4
+        2.19
+        3.79
+        2.39
+        2.33
+        4.2
+        1.53
+        0.0
+        0.0
+    |]
 
-(populations, testPerson2) ||> showEthnoPlotSampleClusterOnly 5
+//(populations, testPersonK13) ||> showEthnoPlot 4
+
+//(populations, testPersonK13) ||> showEthnoPlotSampleClusterOnly 5
+
+[testPersonK15; testPerson2K15; testPerson3K15] |> showSamplesOnEthnoPlot 5 populations
+
+[testPersonK15; testPerson2K15; testPerson3K15] |> showSamplesOnEthnoPlotOwnClustersOnly 5 populations
+
+//[testPersonK36; testPerson2K36; testPerson3K36] |> showSamplesOnEthnoPlot 5 populations
+
+//[testPersonK36; testPerson2K36; testPerson3K36] |> showSamplesOnEthnoPlotOwnClustersOnly 5 populations
+
+
+let son =
+    [|
+        0.28
+        0.0
+        0.0
+        0.0
+        0.0
+        6.58
+        0.0
+        0.0
+        6.09
+        0.0
+        28.15
+        0.0
+        22.61
+        13.13
+        0.0
+        7.29
+        0.0
+        4.45
+        0.0
+        0.0
+        0.0
+        3.74
+        0.34
+        4.05
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        0.24
+        0.26
+        0.0
+        2.76
+        0.0
+        0.0
+        0.0
+    |]
+
+let father =
+    [|
+        0.63
+        0.0
+        1.21
+        0.0
+        0.0
+        8.59
+        0.0
+        0.0
+        6.4
+        0.0
+        25.37
+        0.0
+        20.59
+        9.61
+        0.0
+        4.87
+        0.0
+        5.06
+        0.0
+        0.0
+        0.0
+        4.66
+        5.33
+        5.53
+        0.0
+        0.15
+        0.0
+        0.0
+        0.0
+        0.0
+        0.1
+        0.0
+        1.86
+        0.0
+        0.0
+        0.0
+    |]
+
+let mother =
+    [|
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        5.61
+        0.0
+        0.0
+        7.89
+        0.0
+        25.5
+        0.0
+        19.4
+        15.34
+        1.68
+        5.77
+        0.0
+        3.4
+        0.0
+        0.0
+        0.0
+        4.82
+        0.0
+        5.36
+        0.0
+        0.0
+        0.0
+        0.0
+        0.0
+        1.82
+        0.0
+        0.0
+        3.41
+        0.0
+        0.0
+        0.0
+    |]
+
+
+[son; father; mother] |> showEthnoPlotSampleClusterOnly 5 populations
+
+Seq.zip3 son father mother
+|> Seq.map (fun (s, f, m) -> System.Math.Abs(s - (f+m)/2.0)) 
+|> Seq.toArray
+
+
+
+
+
+//(populations, testPerson2) ||> showEthnoPlotSampleClusterOnly 5
 
--- Admixture/EthnoPlots.fs
+++ Admixture/EthnoPlots.fs
@@ -5,6 +5,23 @@ open FSharp.Charting
 open Accord.MachineLearning
 open Admixture.Populations
 
+let abbreviations =
+        [ 
+            ("Russian", "RU")
+            ("Belarusian", "BY")
+            ("Belarussian", "BY")
+            ("Belorussian", "BY")
+            ("Ukrainian", "UA")
+            ("Southwest", "SW")
+            ("Southeast", "SE")
+            ("Northwest", "NW")
+            ("Northeast", "NE")
+            ("North", "N")
+            ("South", "S")
+            ("West", "W")
+            ("East", "E")
+        ]
+
 let getTransform data =
     let pca = new PrincipalComponentAnalysis()
 
@@ -13,122 +30,153 @@ let getTransform data =
     pca.NumberOfOutputs <- 2;
     pca.Learn(data) 
 
-let getClusters k data =
+let getClusters (k:int) data =
     Accord.Math.Random.Generator.Seed <- new System.Nullable<int>(0);
-    let kmeans = new KMeans(k);
-    kmeans.Learn(data);
+    
+    let kmeans = BalancedKMeans k
+    kmeans.MaxIterations <- 10000
 
+    kmeans.Learn(data)
 
-let getCharts (labels:string[]) (output:float[][]) (clusters:int[]) =
+let getPoints scaleX scaleY (s:_[][]) =
+    s |> Array.map (fun x -> x.[0] * scaleX, x.[1] * scaleY)
+
+let getCharts (labels:string[]) (output:(float*float)[]) (clusters:int[]) =
     output
     |> Array.mapi (fun i x -> (i, x))
     |> Seq.groupBy (fun (i, _) -> clusters.[i])
     |> Seq.map 
         (fun (k, g) -> 
             let ls = g |> Seq.map (fun (i, _) -> labels.[i])
-            k, Chart.Point(g |> Seq.map (fun (_, x) -> -x.[0], -x.[1]), Labels = ls)
+            k, Chart.Point(g |> Seq.map snd, Labels = ls)
         )
 
-let showEthnoPlotOnly k populations =
+let showEthnoPlot k scaleX scaleY components populations =
 
     let data = populations |> Array.map (fun x -> x.Components)
 
-    let transform = getTransform data
+    let cdata = components |> Array.mapi (fun ci _ -> [| for i in 0..components.Length - 1 -> if i = ci then 100.0 else 0.0 |])
 
-    let output = transform.Transform(data)
+    let transform = data |> Array.append cdata |> getTransform
 
-    let clusters = getClusters k data
+    let output = data |> transform.Transform |> getPoints scaleX scaleY
+
+    let clusters = data |> getClusters k
  
-    let clusterLabels = clusters.Decide(data);
-
-    let abbreviations =
-        [ 
-            ("Russian", "RU")
-            ("Belarusian", "BY")
-            ("Belarussian", "BY")
-            ("Ukrainian", "UA")
-            ("North_", "N. ")
-            ("Southwest_", "SW. ")
-            ("West_", "W. ")
-        ]
+    let clusterLabels = data |> clusters.Decide
 
     let labels = populations |> Array.map (fun x -> x.Label)  |> Array.map (fun x -> abbreviations |> Seq.fold (fun (acc : string) (l, s) -> acc.Replace(l, s)) x)
     
+    let cPoints = cdata |> transform.Transform |> getPoints scaleX scaleY
+
     (output, clusterLabels)
-    ||> getCharts labels
+    ||> getCharts labels 
     |> Seq.map snd
+    |> Seq.append
+        [
+            Chart.Point(cPoints, Labels = components) |> Chart.WithSeries.Style(System.Drawing.Color.Red) 
+        ]
     |> Chart.Combine
     |> Chart.WithXAxis(MajorGrid = ChartTypes.Grid(Enabled=false)) 
     |> Chart.WithYAxis(MajorGrid = ChartTypes.Grid(Enabled=false))
     |> Chart.Show
 
-let showEthnoPlot k populations (sample:float[]) =
+let showEthnoPlotByClusters k populations =
+
+    let scaleX = 1.0
+
+    let scaleY = 1.0
 
     let data = populations |> Array.map (fun x -> x.Components)
 
-    let transform = getTransform data
+    let transform = data |> getTransform
 
-    let output = transform.Transform(data)
+    let output = data |> transform.Transform |> getPoints scaleX scaleY
 
-    let point = transform.Transform(sample)
-
-    let clusters = getClusters k data
+    let clusters = data |> getClusters k
  
-    let clusterLabels = clusters.Decide(data);
+    let clusterLabels = data |> clusters.Decide
 
-    let abbreviations =
-        [ 
-            ("Russian", "RU")
-            ("Belarusian", "BY")
-            ("Belarussian", "BY")
-            ("Ukrainian", "UA")
-            ("North_", "N. ")
-            ("Southwest_", "SW. ")
-            ("West_", "W. ")
-        ]
 
     let labels = populations |> Array.map (fun x -> x.Label)  |> Array.map (fun x -> abbreviations |> Seq.fold (fun (acc : string) (l, s) -> acc.Replace(l, s)) x)
+
     
     (output, clusterLabels)
     ||> getCharts labels
-    |> Seq.map snd
-    |> Seq.append
-        [
-            Chart.Point([(-point.[0], -point.[1])], Labels = ["Me"]) |> Chart.WithSeries.Style(System.Drawing.Color.Red)
-        ]
-    |> Chart.Combine
-    |> Chart.WithXAxis(MajorGrid = ChartTypes.Grid(Enabled=false)) |> Chart.WithYAxis(MajorGrid = ChartTypes.Grid(Enabled=false))
-    |> Chart.Show
+    |> Seq.sortBy fst
+    |> Seq.iter (fun (k, c) -> 
+            c 
+            |> Chart.WithTitle(Text = sprintf "Cluster %d" (k+1)) 
+            |> Chart.WithXAxis(MajorGrid = ChartTypes.Grid(Enabled=false)) 
+            |> Chart.WithYAxis(MajorGrid = ChartTypes.Grid(Enabled=false))
+            |> Chart.Show)
 
-let showEthnoPlotSampleClusterOnly k populations (sample:float[]) =
+let showSamplesOnEthnoPlot k populations (samples:seq<float[]>) =
 
     let data = populations |> Array.map (fun x -> x.Components)
 
     let transform = getTransform data
 
-    let output = transform.Transform(data)
+    let output = data |> transform.Transform |> getPoints 1.0 1.0
 
-    let point = transform.Transform(sample)
+    let clusters = data |> getClusters k
+ 
+    let clusterLabels = data |> clusters.Decide
 
-    let clusters = getClusters k data
+    let sampleData = samples |> Seq.toArray
 
-    let clusterLabels = clusters.Decide(data);
+    let sampleOutput = sampleData |> transform.Transform
 
-    let labels = populations |> Array.map (fun x -> x.Label)
+    let labels = populations |> Array.map (fun x -> x.Label)  |> Array.map (fun x -> abbreviations |> Seq.fold (fun (acc : string) (l, s) -> acc.Replace(l, s)) x)
+    
+    let populationCharts = (output, clusterLabels) ||> getCharts labels |> Seq.map snd
 
-    let sampleCluster = clusters.Decide(sample)
+    let samplePoints =
+        sampleOutput |> Seq.map (fun x -> x.[0], x.[1])
 
-    (output, clusterLabels)
-    ||> getCharts labels
-    |> Seq.where (fun (k, _) -> k = sampleCluster)
-    |> Seq.map snd
-    |> Seq.append
-        [
-            Chart.Point([(-point.[0], -point.[1])], Labels = ["Me"]) |> Chart.WithSeries.Style(System.Drawing.Color.Red)
-        ]
+    let sampleLabels =
+        sampleOutput |> Seq.mapi (fun i _ -> sprintf "Test Person %d" ( i + 1))
+
+    [
+        Chart.Point(samplePoints, Labels = sampleLabels) |> Chart.WithSeries.Style(System.Drawing.Color.Red)
+    ]
+    |> Seq.append populationCharts 
     |> Chart.Combine
     |> Chart.WithXAxis(MajorGrid = ChartTypes.Grid(Enabled=false)) |> Chart.WithYAxis(MajorGrid = ChartTypes.Grid(Enabled=false))
     |> Chart.Show
 
+let showSamplesOnEthnoPlotOwnClustersOnly k populations (samples:seq<float[]>) =
 
+    let data = populations |> Array.map (fun x -> x.Components)
 
+    let transform = getTransform data
+
+    let output = data |> transform.Transform |> getPoints 1.0 1.0
+
+    let clusters = data |> getClusters k
+ 
+    let clusterLabels = data |> clusters.Decide
+
+    let sampleData = samples |> Seq.toArray
+
+    let sampleOutput = sampleData |> transform.Transform |> getPoints 1.0 1.0
+
+    let sampleClusters = sampleData |> clusters.Decide
+
+    let labels = populations |> Array.map (fun x -> x.Label)
+
+    let populationCharts = 
+        (output, clusterLabels) 
+        ||> getCharts labels
+        |> Seq.where (fun (k, _) -> sampleClusters |> Seq.contains k) |> Seq.map snd
+
+    let sampleLabels =
+        sampleOutput |> Seq.mapi (fun i _ -> sprintf "Test Person %d" ( i + 1))
+
+    [
+        Chart.Point(sampleOutput, Labels = sampleLabels) |> Chart.WithSeries.Style(System.Drawing.Color.Red)
+    ]
+    |> Seq.append populationCharts
+    |> Chart.Combine
+    |> Chart.WithXAxis(MajorGrid = ChartTypes.Grid(Enabled=false)) |> Chart.WithYAxis(MajorGrid = ChartTypes.Grid(Enabled=false))
+    |> Chart.Show
